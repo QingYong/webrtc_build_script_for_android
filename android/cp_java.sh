dac4d95997334cfb844ccc4b31ac157e746fa273
Zhihan He <zhihanhe@li410-51.members.linode.com>
2016-11-10 07:47:23 +0000

    Add java code collector for AppRTCDemo

---
+++ android/cp_java.sh
@@ -0,0 +1,5 @@
+cp -r ./webrtc/src/webrtc/modules/audio_device/android/java/src ./java
+cp -r ./webrtc/src/webrtc/api/android/java/src ./java
+cp -r ./webrtc/src/webrtc/examples/androidapp/src ./java
+cp -r ./webrtc/src/webrtc/base/java/src ./java
+tar -cvf java.tar.gz ./java
